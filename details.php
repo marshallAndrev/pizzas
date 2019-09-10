67a770fb79326707505690b85f083f0ef5e95a70
marshallAndrev <kontakt.marcinwojcik@gmail.com>
2019-09-10 21:42:52 +0200

    Getting single record from database

--- details.php
+++ details.php
@@ -1,20 +1,22 @@
 <?php
-
+	
 	include('config/db_connect.php');
 
 	//check GET request id param
 	if (isset($_GET['id'])) {
 		$id = mysqli_real_escape_string($conn, $_GET['id']);
 
-	//make sql
-	$sql = "SELECT * FROM pizzas WHERE id = $id";
+		//make sql
+		$sql = "SELECT * FROM pizzas WHERE id = $id";
 
-	//get the query result
-	$result = mysqli_query($conn, $sql);
+		//get the query result
+		$result = mysqli_query($conn, $sql);
 
-	//fetch result in array format
-	$pizza = mysqli_fetch_assoc($result);
+		//fetch result in array format
+		$pizza = mysqli_fetch_assoc($result);
 
+		mysqli_free_result($result);
+		mysqli_close($conn);
 
 	}
 
@@ -25,7 +27,22 @@
 
 <?php include('templates/header.php'); ?>
 
-<h2>More info</h2>
+<div class="container center">
+	<?php if ($pizza): ?>
+	
+		<h4><?php echo htmlspecialchars($pizza['title']); ?></h4>
+		<p>Created by: <?php echo htmlspecialchars($pizza['email']); ?></p>
+		<p><?php echo date($pizza['created_at']); ?></p>
+		<h5>Ingredients:</h5>
+		<p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+
+	<?php else: ?>
+
+		<h5>No such pizza exist!</h5>
+
+	<?php endif; ?>
+	
+</div>
 
 <?php include('templates/footer.php'); ?>
 
