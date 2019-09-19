2c97938f14c2422bd25586f438795a063c7e246e
Marcin Wojcik <kontakt.marcinwojcik@gmail.com>
2019-09-19 23:02:19 +0200

    Add session

---
+++ sandbox.php
@@ -0,0 +1,28 @@
+<?php
+
+	if (isset($_POST['submit'])) {
+		
+		session_start();
+
+		$_SESSION['name'] = $_POST['name'];
+
+		header('Location: index.php');
+	}
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Php tuts</title>
+</head>
+<body>
+	<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+		<input type="text" name="name">
+		<input type="submit" name="submit" value="submit">
+	</form>
+</body>
+</html>
--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,13 @@
+<?php
+
+    session_start();
+
+    $_SESSION['name'] = 'yoshi';
+
+    $name = $_SESSION['name'];
+
+?>
+
 <head>
 	<title>Ninja Pizza</title>
 	 <!-- Compiled and minified CSS -->
@@ -14,6 +24,13 @@
     		margin: 20px auto;
     		padding: 20px;
     	}
+        .pizza {
+            width: 100px;
+            margin: 40px auto -30px;
+            display: block;
+            position: relative;
+            top: -30px;
+        }
     </style>
 </head>
 	<body class="grey lighten-4">
@@ -21,6 +38,7 @@
 			<div class="container">
 				<a href="index.php" class="brand-logo brand-text">Ninja Pizza</a>
 				<ul id="nav-mobile" class="right hide-on-small-and-down">
+                    <li class="grey-text">Hello <?php echo htmlspecialchars($name); ?></li>
 					<li><a href="add.php" class="btn brand z-dept-0">Add a Pizza</a></li>
 				</ul>
 			</div>
